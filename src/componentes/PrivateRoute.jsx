2aae9dda925b73f124053cb6e8cbc1595ee1f40a
Florencia Da Rosa <floordarosa2002gmail.com>
2023-07-07 14:56:57 -0300

    se agregan cambios

--- src/componentes/PrivateRoute.jsx
+++ src/componentes/PrivateRoute.jsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ component: Component, user, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        user ? (
-          <Component {...props} />
-        ) : (
-          <Navigate to="/register" />
-        )
-      }
-    />
-  );
-};
+function PrivateRoute({ component: Component, ...rest }) {
+    const isUserAuthenticated = useAuth(); 
+    return (
+      <Route
+        {...rest}
+        element={isUserAuthenticated ? <Component /> : <Navigate to="/register" />}
+      />
+    );
+  }  
 
 export default PrivateRoute;
