bc8198c04b9b98fde16c4a17b770a38b619fd432
Florencia Da Rosa <floordarosa2002gmail.com>
2023-07-07 15:07:48 -0300

    se agrega

--- src/componentes/PrivateRoute.jsx
+++ src/componentes/PrivateRoute.jsx
@@ -1,11 +1,16 @@
-function PrivateRoute({ component: Component, ...rest }) {
-    const isUserAuthenticated = useAuth(); 
-    return (
-      <Route
-        {...rest}
-        element={isUserAuthenticated ? <Component /> : <Navigate to="/register" />}
-      />
-    );
-  }  
+ import React from 'react';
+    import { Route, Navigate } from 'react-router-dom';
+    import { useAuth } from './Auth'; // Importa la función useAuth desde el archivo Auth.js
+    
+    function PrivateRoute({ component: Component, ...rest }) {
+      const isUserAuthenticated = useAuth(); // Utiliza la función useAuth() para verificar si el usuario está autenticado.
+    
+      return (
+        <Route
+          {...rest}
+          element={isUserAuthenticated ? <Component /> : <Navigate to="/register" />}
+        />
+      );
+    }
 
 export default PrivateRoute;
