70799fcac35ab0414ec431e70402b03a7f80ed13
Florencia Da Rosa <floordarosa2002gmail.com>
2023-07-06 21:58:25 -0300

    rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Nosotros from './componentes/Nosotros';
 import Users from './componentes/Users';
 import Login from './componentes/Login';
 import Registro from './componentes/Registro';
 import Contacto from './componentes/Contacto';
 import List from './componentes/List';
-import { UserProvider} from './UserContext'
-import './App.css'
-
+import { UserProvider } from './UserContext';
+import './App.css';
 
 
 function App() {
   return (
     <UserProvider>
       <Routes>
-        <Route exact path='/' element={<Login />} />
-        <Route exact path='/nosotros' element={<Nosotros />} />
-        <Route exact path='/register' element={<Registro />} />
-        <Route exact path='/users' element={<Users />} />
-        <Route exact path='/contacto' element={<Contacto />} />
-        <Route exact path='/list' element={<List />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/register" element={<Registro />} />
+        <Route
+          path="/users"
+          element={<PrivateRoute component={Users} />}
+        />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route
+          path="/list"
+          element={<PrivateRoute component={List} />}
+        />
       </Routes>
     </UserProvider>
-  )
+  );
 }
 
-export default App
+function PrivateRoute({ component: Component, ...rest }) {
+  const isUserAuthenticated = true; 
+  return (
+    <Route
+      {...rest}
+      element={isUserAuthenticated ? <Component /> : <Navigate to="/register" />}
+    />
+  );
+}
+
+export default App;
