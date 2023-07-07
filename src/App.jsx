c8a2175ec076bfc35e5c79a431926246bf5ce09e
EnriqueKloosterman <kloostermanen@gmail.com>
2023-07-07 17:14:30 -0300

    reaparando archivo

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import { Routes, Route } from 'react-router-dom';
+import Nosotros from './componentes/Nosotros';
+import Users from './componentes/Users';
+import Login from './componentes/Login';
+import Registro from './componentes/Registro';
+import Contacto from './componentes/Contacto';
+import List from './componentes/List';
+import { UserProvider } from './UserContext';
+import './App.css';
+import ProtectedRoute from './auth/ProtectedRoute';
+
+function App() {
+    return (
+        <UserProvider>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/nosotros" element={<Nosotros />} />
+                <Route path="/register" element={<Registro />} />
+                <Route path="/contacto" element={<Contacto />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/list" element={<List />} />
+                </Route>
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/users" element={<Users />} />
+                </Route>
+                <Route path="*" element={<h2>Not Found</h2>} />
+            </Routes>
+        </UserProvider>
+    );
+}
+
+export default App;
